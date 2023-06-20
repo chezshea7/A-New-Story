2f1480053cf099d428b387037c59df65de37ad60
chezshea7 <136706102+chezshea7@users.noreply.github.com>
2023-06-20 16:59:21 +0700

    Fix issue reported by user

---
+++ yellow_unicorn_flower.java
@@ -0,0 +1,27 @@
+public class NewStory { 
+  
+    public static void main(String[] args) { 
+      
+        System.out.println("Once upon a time, there lived a boy named Max."); 
+        System.out.println("Max lived in a small village close to a forest."); 
+        System.out.println("Max's village was very peaceful but the forest was mysterious.");
+        System.out.println("Max had heard stories from his parents and elders about strange things happening in the forest."); 
+        System.out.println("Curious about the truth, one day Max decided to go explore the forest.");
+        System.out.println("Max ventured into the forest and immediately felt a chill run down his spine."); 
+        System.out.println("Everything seemed so quiet and he could not see or hear any living being."); 
+        System.out.println("He decided to go deeper into the forest in search of the mysteries.");
+        System.out.println("After walking for a while, he noticed a light in the distance."); 
+        System.out.println("He decided to follow the light and eventually reached a clearing."); 
+        System.out.println("At the clearing, he saw a group of fairies dancing around a campfire."); 
+        System.out.println("He stayed there for a long time, mesmerized by the beauty of the fairies.");
+        System.out.println("They eventually noticed his presence and invited him to join their dance."); 
+        System.out.println("Max happily accepted and soon he was part of the fairy circle, dancing and having a great time.");
+        System.out.println("After that, Max visited the forest every day and made many new friends."); 
+        System.out.println("He kept visiting the forest and soon enough, he knew all the secrets of the mysterious forest."); 
+        System.out.println("Max's parents and elders were surprised to see him so knowledgeable about the secrets of the forest.");
+        System.out.println("From then on, Max became the village's go-to person for understanding the secrets of the forest."); 
+        System.out.println("Max and the fairies had become close friends, and they never parted."); 
+        System.out.println("Max was a source of great happiness to the village.");
+        System.out.println("And they all happily lived ever after."); 
+    } 
+}
