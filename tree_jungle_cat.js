839d51343406a8234e8eb505d4971d5c93dd7526
chezshea7 <136706102+chezshea7@users.noreply.github.com>
2023-06-28 03:35:06 +0700

    Fix edge case scenario

---
+++ tree_jungle_cat.js
@@ -0,0 +1,47 @@
+let character = "John";
+let setting = "jungle";
+let animals = ["lions", "tigers", "bears"];
+
+let story = `Once upon a time there was a brave explorer called ${character}.
+He was exploring the depths of a ${setting} when he stumbled
+upon a group of ${animals[0]}, ${animals[1]}, and  ${animals[2]}.
+
+John was not afraid and he continued on his journey, defying danger at every turn.
+He discovered ancient temples and mystical creatures. He made friends with the locals
+and explored every corner of this magical place.
+
+He eventually found himself in a clearing with a large lake at the center.
+As he looked out across the lake, he saw a large stone structure in the distance.
+He decided to investigate further and he soon realized that it was the entrance
+to an underground cavern.
+
+John traveled further into the depths of the cave, following a winding path
+until he reached a large chamber. Inside he found a beautiful marble statue
+of a dragon. As John looked upon the statue, he realized that this was no ordinary
+dragon: it was a powerful guardian of the cave.
+
+John slowly approached the statue and bowed his head in respect. He could feel
+the immense power emanating from the dragon. Suddenly, a voice filled the chamber
+that said "You have come here to seek my power, brave explorer. I will grant you one wish
+but you must answer one question. What would you choose as your prize?"
+
+John thought carefully before answering, "I choose knowledge, that I may gain wisdom
+and understanding, so I can protect the people that I love."
+
+The dragon smiled and said "Very well. I grant you the knowledge you seek."
+Suddenly, John felt a wave of understanding wash over him and he realized that he
+had gained the power to manipulate the elements. He thanked the dragon and quickly
+left the cave, eager to use his new power to help those that he cared for.
+
+John returned to his home and was surprised to find that his village was in ruins.
+He quickly set to work using his newfound abilities to restore the village to its
+former glory. Soon, the village was even better than it was before.
+
+John was celebrated as a hero and people from across the land came to admire him and
+hear his tale. He used his newfound knowledge to protect his people and lead them
+toward a brighter future.
+
+And so, with a brave explorer and a powerful dragon, a new story began.
+`;
+
+console.log(story);
