eda0cfefa7f8aa487f9f2e9431a9bf5cfbe30c40
chezshea7 <136706102+chezshea7@users.noreply.github.com>
2023-06-28 08:59:53 +0700

    Add error handling for edge cases

---
+++ dog_piano_jungle.js
@@ -0,0 +1,21 @@
+//A New Story
+
+var story = "Once upon a time there was a small kingdom perched on the edge of a distant mountain. The people of the kingdom were proud and content, living lives of leisure and ease. 
+
+One day, the people of the kingdom heard a strange sound emanating from the mountain. They soon discovered that a great dragon lived in a cave atop the mountain and the sound they heard was the dragon's thunderous roar. 
+
+The people of the kingdom became terrified of the dragon and were desperate to get rid of it. So, they decided to form a plan to kill the beast. The bravest and strongest man of the kingdom volunteered to climb to the top of the mountain and slay the dragon.
+
+The man set off on his quest and after days of climbing he finally reached the cave where the dragon slept. Carefully, he approached the dragon, who was deeply asleep. The man raised his sword and was just about to strike, when suddenly the dragon opened its eyes. 
+
+Seeing that the dragon was not as frightful as he had imagined, the man decided to spare its life. The dragon was surprised and asked the man why he chose not to kill it. 
+
+The man told the dragon that he could not bring himself to harm something that was so majestic and powerful. The dragon, who was touched by the man's words, offered a deal. It said that if the man agreed to be its loyal servant, it would grant him three wishes in return. 
+
+The man agreed to the dragon's proposal and was filled with happiness. He could not believe that his quest had led him to such fortune. With the help of the dragon, he fulfilled his dreams and the dreams of the kingdom. 
+
+The man and the dragon lived in harmony for many years to come. The people of the kingdom were grateful for their newfound peace and prospered under the dragon's protection. 
+
+And they all lived happily ever after.
+
+//End of Story
