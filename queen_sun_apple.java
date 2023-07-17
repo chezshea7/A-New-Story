06be4c73e0cd29e74574df9cbf81e7f9c15f8abd
chezshea7 <136706102+chezshea7@users.noreply.github.com>
2023-07-17 12:36:39 +0700

    Enhance input validation

---
+++ queen_sun_apple.java
@@ -0,0 +1,65 @@
+public class ANewStory {
+
+	public static void main(String[] args) {
+		
+		String storyline = "Once upon a time in a far away land, there lived a beautiful princess. The princess had long, "
+				+ "golden hair and the kindest heart.  Everyone in the kingdom loved her dearly.\n";
+		
+		// 1
+		storyline += "One day, the princess was walking along a pathway when she heard a voice coming from the forest.\n";
+		
+		// 2
+		storyline += "The voice was that of a young man with a gentle and kind demeanor. She stopped to listen to what he had to say.\n";
+		
+		// 3
+		storyline += "The young man asked the princess to follow him deep into the forest, as he had something important to show her.\n";
+		
+		// 4
+		storyline += "The princess was curious and decided to follow the young man. She followed him until they reached a clearing in the forest.\n";
+		
+		// 5
+		storyline += "The young man stepped aside, revealing a beautiful garden full of rare and exotic plants. He told the princess that this was her destiny.\n";
+		
+		// 6
+		storyline += "The young man explained that the garden had been created just for her, and that if she took care of it, it would bring her untold joy and happiness.\n";
+		
+		// 7
+		storyline += "The young man also said that if she ever needed help, he would be there for her. With that, he vanished into the forest.\n";
+		
+		// 8
+		storyline += "The princess was amazed by what she had seen, and she decided to take care of the garden. She soon became very attached to it.\n";
+		
+		// 9
+		storyline += "Every day, the princess would tend to the garden, and it flourished under her care. People from all over the kingdom would come to see the beautiful garden.\n";
+		
+		// 10
+		storyline += "The garden became so beautiful that the princess earned the admiration of everyone in the kingdom. She was soon known as the Princess of the Garden.\n";
+		
+		// 11
+		storyline += "One day, the princess heard the same voice from before coming from the forest. She ventured into the forest to find the young man.\n";
+		
+		// 12
+		storyline += "When she found him, he told her that it was time for her to move on and that he could not stay with her any longer. The princess was saddened by this news.\n";
+		
+		// 13
+		storyline += "The young man then gave the princess a gift. It was a magical seed that could bring life to anything that it was planted in.\n";
+		
+		// 14
+		storyline += "The princess thanked the young man for his gift and they said their goodbyes. The princess returned to the kingdom with the magical seed.\n";
+		
+		// 15
+		storyline += "The princess planted the magical seed in the garden and, to her surprise, it blossomed into a majestic tree. It was such a sight to behold that it moved the princess to tears.\n";
+		
+		// 16
+		storyline += "The tree became a symbol of hope and a reminder that the princess would never be alone. From that day forward, the princess was known as the Princess of the Tree.\n";
+		
+		// 17
+		storyline += "And so the princess continued to take care of the garden, and it continued to flourish. She never forgot the young man who had changed her life and brought her so much joy.\n";
+		
+		// 18
+		storyline += "To this day, the Princess of the Tree and the magical garden continue to bring joy and hope to the people of the kingdom.\n";
+		
+		System.out.println(storyline);
+	}
+
+}
